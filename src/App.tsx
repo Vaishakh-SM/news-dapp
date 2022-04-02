f09f7cdcc7d59c678f8731dfda230a1297a6f215
Vaishakh <vaishakhsm@gmail.com>
2022-04-02 13:43:40 +0530

    Added ethers

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { ethers } from "ethers";
 import logo from './logo.svg'
 import './App.css'
 
+
+const trial = async(provider: { getBlockNumber: () => any; })=>{
+  const data = await provider.getBlockNumber()
+  console.log("Data is ",data)
+}
+
+const trial2 = async()=>{
+  const provider = new ethers.providers.Web3Provider((window as any).ethereum, "any");
+// Prompt user for account connections
+  await provider.send("eth_requestAccounts", []);
+  const signer = provider.getSigner();
+  console.log("Account:", await signer.getAddress());
+}
+
 function App() {
   const [count, setCount] = useState(-3)
+  useEffect(()=>{
+//     const provider = new ethers.providers.JsonRpcProvider();
+//     console.log("Provider is ",provider)
+
+// // The provider also allows signing transactions to
+// // send ether and pay to change state within the blockchain.
+// // For this, we need the account signer...
+//     const signer = provider.getSigner()
+//     trial(provider);
+        trial2();
+  },[])
 
   return (
     <div className="App">
